2b79a0e1bb399ad0f9bf3150e7de834eff063f0c
eileenlcb <524840526@qq.com>
2023-09-20 01:29:31 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-
-    println!("Hello, world!");
+#![allow(unused)]
+use clap::{Parser, ValueEnum};
+#[derive(Parser, Debug)]
+struct Args{
+    #[arg(short, long, value_name="item-name")]
+    add: Option<String>,
 }
 
+enum ListType{
+    All,
+    Completed,
+    Uncompleted,
+    Deleted,
+}
 
+fn main(){
+    let args = Args::parse();
+    println!("{:?}", args);
+}
