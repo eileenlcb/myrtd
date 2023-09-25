9fa6a661684acb5e22a575d0e81331f8ce19462c
eileenlcb <524840526@qq.com>
2023-09-25 23:58:45 +0800

    some nots

--- src/storage.rs
+++ src/storage.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 use std::fmt::Display;
 use std::fs::File;
 use std::fs::{self, OpenOptions};
-use std::io::{self,BufReader, BufWriter, Read, Seek, Write};
+use std::io::{self,BufReader, BufWriter, Read, Seek, Write, BufRead};
 use std::path::Path;
 
 const CSV_FILE_NAME: &str = ".rtd.csv";
@@ -12,6 +12,7 @@ type Result<T> = std::result::Result<T, io::Error>;
 
 pub fn add_item(item:Item) -> Result<()>{
     let mut csv = Csv::new()?;
+    //将文件指针移动到末尾
     csv.file.seek(std::io::SeekFrom::End(0))?;
     writeln!(csv.file, "{}", item.to_string())?;
     Ok(())
@@ -23,13 +24,27 @@ struct Csv{
     file:File,
 }
 
+// pub fn get_max_id() -> Result<u32>{
+//     let max_id = 
+// }
+
+pub fn get_all() -> Result<Vec<Item>>{
+    Ok(Csv::new()?
+        .content()?
+        .lines()
+        .filter_map(|line| line.parse::<Item>().ok())
+        .collect())
+}
+
 impl Csv {
     fn new() -> Result<Self>{
         let filename = Csv::filename()?;
         let path  = Path::new(&filename);
+        println!("path is in {:?}",path);
 
         if !path.exists(){
             let mut file = Csv::create(path)?;
+            //将字符串字面量理解为字节序列，不会进行utf-8编码，适用于文件写入
             file.write_all(b"id,name,completed,deleted,createdAt,completedAt,deletedAt\n")?;
             Ok(Self{
                 filename:filename.to_string(),
@@ -65,6 +80,12 @@ impl Csv {
         let home = env::var("HOME").unwrap();
         let filename = home + "/" + CSV_FILE_NAME;
         Ok(filename)
+    }
+
+    fn content(&mut self) -> Result<String>{
+        let mut content = String::new();
+        self.file.read_to_string(&mut content)?;
+        Ok(content)
     }
 }
 
